d9e66bf5d3611f75363176c40686913b0913b0d1
“yanxiangrong“ <yanxiangrong_2@outlook.com>
2023-04-28 15:54:11 +0800

    Revert "🙂"
    This reverts commit cd39a3c5a7a0ff7b5a857356fd57fc672e94314e.

--- bed/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class UploadFileForm(forms.Form):
-    file = forms.ImageField(max_length=10*1204*1024)
--- example/apps.py
+++ example/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BedConfig(AppConfig):
+class ExampleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bed'
+    name = 'example'
