14974cd79bebfd1fa362d3b78e98c2f2d9860406
“yanxiangrong“ <yanxiangrong_2@outlook.com>
2023-04-28 15:56:35 +0800

    🙂

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example'
+    'bed.apps.BedConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = 'media/'
