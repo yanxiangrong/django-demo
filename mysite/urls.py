cd39a3c5a7a0ff7b5a857356fd57fc672e94314e
“yanxiangrong“ <yanxiangrong_2@outlook.com>
2023-04-28 15:52:05 +0800

    🙂

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
-"""vercel_app URL Configuration
+"""
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include,path
 
 urlpatterns = [
+    path("", include("bed.urls")),
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
 ]
